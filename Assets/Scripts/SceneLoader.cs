ead5c065664c2d5e4c9f0ad1c381bb12c00358ad
Leigham <Leighamss@hotmail.com>
2019-07-14 16:29:57 -0400

    SceneLoaderAdded

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadNextScene()
+    {
+        
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex + 1);
+       
+    }
+
+    public void LoadSceneOne()
+    {
+        //FindObjectOfType<GameSession>().ResetGame();
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); Application.Quit();
+    }
+}
